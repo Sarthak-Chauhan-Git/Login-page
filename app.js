661f5a1df572328754b5930fd3f74b37f28c1eb4
Sarthak-Chauhan-Git <sarthakchauhan.official@gmail.com>
2025-06-02 16:39:56 +0530

    Registration form data fetching done

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const form = document.querySelector("form");
 const newAccount = document.querySelector(".newAccount");
 const container = document.querySelector(".container");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const signup = document.querySelector("#signup");
+const signup = document.querySelector(".signup");
 const loginPart = document.querySelector(".part2");
 const registerImage = document.querySelector(".part1");
 const registerArea = document.querySelector(".part3");
@@ -13,18 +10,12 @@ newAccount.classList.remove("show");
 let hoverTimeout;
 let isSignin = true;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(`Username: ${username.value} Password: ${password.value}`);
-  password.value = "";
-});
-
+// toggle button movement
 container.addEventListener("mouseenter", () => {
   hoverTimeout = setTimeout(() => {
     newAccount.classList.add("show");
   }, 1000);
 });
-
 container.addEventListener("mouseleave", () => {
   if (isSignin) {
     clearTimeout(hoverTimeout);
@@ -32,14 +23,67 @@ container.addEventListener("mouseleave", () => {
   }
 });
 
+// transition toggle
 signup.addEventListener("click", () => {
-  isSignin = false;
-  loginPart.classList.add("hidden");
-  registerImage.classList.remove("part1");
-  registerImage.classList.add("part1after");
-  // signup.style.xtranslate = "-6rem";
-  signup.hidden = true;
-  setTimeout(() => {
+  if (isSignin) {
+    isSignin = false;
+    loginPart.classList.add("hidden");
+    registerImage.classList.remove("part1");
+    registerImage.classList.add("part1after");
+    signup.classList.remove("signup");
+    signup.classList.add("signin");
+    signup.innerHTML = "Already have an account?";
     registerArea.classList.remove("hidden");
-  });
+  } else {
+    isSignin = true;
+    loginPart.classList.remove("hidden");
+    registerImage.classList.remove("part1after");
+    registerImage.classList.add("part1");
+    signup.classList.remove("signin");
+    signup.classList.add("signup");
+    signup.innerHTML = "Don't have an account?";
+    registerArea.classList.add("hidden");
+  }
+});
+
+// SignIn Form data
+const form1 = document.querySelector(".signinForm");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+form1.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!username.value || !password.value) {
+    alert("Please enter both username and password.");
+  } else {
+    console.log(`Username: ${username.value} Password: ${password.value}`);
+    password.value = "";
+  }
+});
+
+// Register Form data
+let sentOTP = "123456"; // dummy OTP
+const form2 = document.querySelector(".registerForm");
+const newUser = document.querySelector("#newUsername");
+const newPass = document.querySelector("#newPassword");
+const phone = document.querySelector("#phone");
+const reOtp = document.querySelector("#resendOTP");
+const otp = document.querySelector("#otp");
+const submit2 = document.querySelector(".btn2");
+form2.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!newUser.value || !newPass.value) {
+    alert("Please enter both username and password.");
+  }else{
+  const newUsername = newUser.value;
+  const newPassword = newPass.value;
+  const newPhone = phone.value;
+  console.log(`Registered as ${newUsername}, ${newPassword} , ${newPhone}`);
+  e.preventDefault();
+  if (otp.value !== sentOTP) {
+    alert("Incorrect OTP");
+    return;
+  }
+  alert("Account created successfully!");
+  newPass.value = "";
+}
 });
